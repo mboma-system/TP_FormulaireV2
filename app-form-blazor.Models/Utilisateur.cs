c8316d928b0bf1e0a37186e9a6a1ece366c9d5df
mboma-system <simopierremboma@gmail.com>
2022-05-02 02:53:11 +0200

    first commit

---
+++ app-form-blazor.Models/Utilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app_form_blazor.Models
+{
+    public class Utilisateur
+    {
+        [Required, RegularExpression(@"^[A-Z]{1}[a-zA-Z\séèë\-_]*$")]
+        public string Nom { get; set; }
+        [Required, RegularExpression(@"^([a-zA-Z0-9\.\-_]+)@([a-zA-Z0-9\-_]+)(\.)?([a-zA-Z0-9\-_]+)?(\.){1}([a-zA-Z]{2,11})$")]
+        public string Email { get; set; }
+        [Required, RegularExpression(@"^([a-zA-Z0-9\.\-_\s]*)$")]
+        public string Adresse { get; set; }
+        [Required, RegularExpression(@"^[0-9]{5}$")]
+        public string CodePostal { get; set; }
+
+        [Required, RegularExpression("^[1-9]{1,125}$")]
+        public int Age { get; set; }
+        [Required]
+        public DateTime DateDeNaissance { get; set; }
+
+        public string CheckingMaritalStatus()
+        {
+            if (IsMarried)
+            {
+                return "Marié(e)";
+            }
+            else { return "Célibataire"; }
+            }
+        public bool IsMarried { get; set; }
+
+        [Required]
+        public string Favoritcolor { get; set; }
+    }
+}
---
+++ app-form-blazor/Pages/Formulaire.razor.cs
@@ -0,0 +1,34 @@
+﻿using app_form_blazor.Models;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace app_form_blazor.Pages
+{
+    public class FormulaireBase : ComponentBase
+    {
+        protected bool afficheCheck = false;
+
+    public Utilisateur user = new ();
+
+        protected override void OnInitialized()
+        {
+
+        }
+        protected override void OnParametersSet()
+        {
+
+        }
+        protected async Task Validate()
+        {
+            afficheCheck = true;
+            await Task.CompletedTask;
+
+        }
+
+
+    }
+
+}
